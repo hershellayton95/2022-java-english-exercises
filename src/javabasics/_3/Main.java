0579bd0291afda2302f087942f1106398284c7db
Greg Mitten <me@greg-mitten.dev>
2022-11-18 15:47:44 +0100

    Java basics 3

---
+++ src/javabasics/_3/Main.java
@@ -0,0 +1,39 @@
+package javabasics._3;
+
+public class Main {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+    }
+
+    /**
+     * 1: print out your initials using System.out.print and then a char literal,
+     * i.e. 'a', 'b', 'c'. You will need multiple print statements
+     */
+    private static void exerciseOne() {
+        // Write your code here
+    }
+
+    /**
+     * 2: Print out your age as an int literal, i.e. 28, then print whether or not you've
+     * had lunch today as a boolean literal i.e. true, false, then print the price of
+     * your lunch as a double, i.e. 4.99
+     */
+    private static void exerciseTwo() {
+        // Write your code here
+    }
+
+    /**
+     * 3: Complete exercise 2, but store the values in a variable.
+     * And then print out the variable.
+     *
+     * i.e.
+     * char favouriteLetter = 'g';
+     * System.out.print("My favourite letter=");
+     * System.out.println(favouriteLetter)
+     */
+    private static void exerciseThree() {
+        // Write your code here
+    }
+}
