3091db8995be3a202e95260458375aa9920c2244
Greg Mitten <me@greg-mitten.dev>
2022-11-25 16:27:07 +0100

    Java Basics 13

---
+++ src/javabasics/_13/Main.java
@@ -0,0 +1,70 @@
+package javabasics._13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+    }
+
+    /**
+     * 1: Add 8 random integers to the array list
+     *
+     *    Print out the first item
+     *
+     *    Remove the first 3 items
+     *
+     *    The print out the first remaining item
+     */
+    public static void exerciseOne() {
+        System.out.println("Exercise 1:");
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        //Write your code here
+    }
+
+    /**
+     * 2: Create 2 Array Lists,
+     *
+     *    A list of strings called 'menuItems'
+     *
+     *    And a list of Doubles called 'menuPrices'
+     *
+     *    The indexes of these two lists correspond to eachother
+     *
+     *    Create a print statement to output the name and price of each
+     *    menu item correspond
+     */
+    public static void exerciseTwo() {
+        System.out.println("\nExercise 1:");
+        //Write your code here
+    }
+
+    /**
+     * 3: Find the total sum of all items in the ArrayList below.
+     *
+     *    Use the + operator
+     *
+     *    print the total
+     */
+    public static void exerciseThree() {
+        System.out.println("\nExercise 3:");
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(5);
+        items.add(10404);
+        items.add(-234348);
+        items.add(-53);
+        items.add(9);
+        items.add(91);
+        items.add(92);
+        items.add(34534);
+        items.add(12334);
+        items.add(1435);
+
+        //Write your code here
+
+    }
+}
