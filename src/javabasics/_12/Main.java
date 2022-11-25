8978da31131c4b8906dfaf7b586bf89b923d381a
Greg Mitten <me@greg-mitten.dev>
2022-11-25 14:51:24 +0100

    Java Basics 11

---
+++ src/javabasics/_12/Main.java
@@ -0,0 +1,30 @@
+package javabasics._12;
+
+public class Main {
+    public static void main(String[] args) {
+        exerciseOne("Java runs on over 2 billion devices");
+        exerciseOne("+31 2 58765446");
+        exerciseOne("hello world");
+    }
+
+    /**
+     * 1: We are going to 'score' strings, given a string you are to calculate its score based on the following factors.
+     *
+     *    Start with a base score of the string's length
+     *    * you can find this out with text.length()
+     *
+     *    If the string is all lowercase, add 10 to the score.
+     *    * You can check this by using .toLowerCase() and .equals()
+     *
+     *    If the string contains any numbers, -5
+     *
+     */
+    public static double exerciseOne(String text) {
+        System.out.println("Exercise 1 - String score:");
+        double yourScore = 0;
+
+        // Write your calculation code here
+
+        return yourScore;
+    }
+}
